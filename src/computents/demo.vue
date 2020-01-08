b37865a3850cc261163e7aae61bca4f970605f22
曹昊 <240135999@qq.com>
2020-01-08 15:18:45 +0800

    完善提交内容

--- src/computents/demo.vue
+++ src/computents/demo.vue
@@ -1,3 +1,21 @@
 <template>
-	<div>123</div>
+  <div>
+   <van-button type="primary">主要按钮</van-button>
+   <div class="aaa">123</div>
+  </div>
 </template>
+<script>
+export default {
+  name:'zujian1',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+<style lang="less">
+.aaa{
+  font-size: 50px
+}
+</style>
---
+++ src/computents/phoneCustom.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="phoneCustom">
+   <!-- <mam-form-item 
+    :decorator='decorator'
+    :label="info.name"> -->
+      <van-field  v-model="value" ></van-field>
+      <div class="aaa">
+        我有一只小毛驴我从来也不骑
+      </div>
+    <!-- </mam-form-item>  -->
+  </div>
+</template>
+<script>
+import { plugMixins , defultComputedMixin} from '../mixins';
+export default {
+  name: 'phoneCustom',
+  // mixins: [plugMixins , defultComputedMixin],
+  // inject:['mamAppForm'],
+  data() {
+    return {
+      value: '111'
+    }
+  },
+  mounted() {
+    console.log('this.mamAppForm----',this.mamAppForm)
+  }
+}
+</script>
+<style lang="less" scoped>
+.aaa{
+  font-size: 30px;
+  padding: 20px;
+}
+</style>
